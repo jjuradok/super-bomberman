272bce4e40a0de0b41edfb9201e23ee7ae6224e6
Juan Manuel Jurado <102827797+jjuradok@users.noreply.github.com>
2024-06-23 15:25:06 -0300

    Create Level class and implementation

--- include/Level.h
+++ include/Level.h
@@ -1,12 +1,24 @@
-#include <vector>
+#ifndef LEVEL_H
+#define LEVEL_H
 
-class Level {
-  const int rows;
-  const int columns;
-  std::string fileName;
-  std::vector<std::vector<int>> matrix(rows, std::vector<int>(columns));
-  public:
-  Level(std::string filename);
+#include <vector>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+class Level
+{
+  string fileName;
+  vector<vector<int>> matrix;
+  void loadMatrix();
+
+public:
+  Level(string filename);
   int getRows();
   int getColumns();
+  vector<vector<int>> getMatrix();
+  void draw(sf::RenderWindow &w);
 };
+
+#endif 
