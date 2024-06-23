d31d0c8b5857dc11f2d5ff8951acfd06bf9e9bbd
Juan Manuel Jurado <102827797+jjuradok@users.noreply.github.com>
2024-06-23 15:25:31 -0300

    add test level src constant

--- include/config/ResourcesLocation.h
+++ include/config/ResourcesLocation.h
@@ -6,5 +6,6 @@ using namespace std;
 const string PLAYER_TEXTURE = "assets/textures/player.png";
 const string DESTRUCTIBLE_BOX_TEXTURE="assets/textures/destructible_box.png";
 const string INDESTRUCTIBLE_BOX_TEXTURE = "assets/textures/indestructible_box.png";
+const string TEST_LEVEL_LAYOUT = "utils/levels/test.txt"; 
 
 #endif
