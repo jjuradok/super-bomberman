5fa3baa8e75730348ba3f4ecb0faefd2fa8ac9da
Juan Manuel Jurado <102827797+jjuradok@users.noreply.github.com>
2024-06-23 15:25:19 -0300

    reorder class atributes and methods

--- include/Partida.h
+++ include/Partida.h
@@ -5,20 +5,20 @@
 #include <vector>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include "Scene.h"
+#include "Level.h"
 using namespace sf;
 using namespace std;
 
 class Partida : public Scene
 {
+	Level level;
+	Tanque m_tanque_p1;
+	Tanque m_tanque_p2;
+	vector<Disparo> m_disparos;
 public:
 	Partida();
 	void update(Game &j) override;
 	void draw(RenderWindow &w) override;
-
-private:
-	Tanque m_tanque_p1;
-	Tanque m_tanque_p2;
-	vector<Disparo> m_disparos;
 };
 
 #endif
