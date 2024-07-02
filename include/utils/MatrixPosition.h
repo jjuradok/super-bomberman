387294778426e442b4bfb5015cdc3eed77ca2b68
Juan Manuel Jurado <102827797+jjuradok@users.noreply.github.com>
2024-07-02 09:45:09 -0300

    add utils

---
+++ include/utils/MatrixPosition.h
@@ -0,0 +1,9 @@
+#ifndef MATRIX_POSITION_H
+#define MATRIX_POSITION_H
+
+struct MatrixPosition
+{
+  int i, j;
+};
+
+#endif
---
+++ include/utils/Vector.h
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+template <typename T>
+
+bool includes(vector<T> v, T value) {
+  for (T i = 0; i < v.size(); i++) {
+    if (v[i] == value) {
+      return true;
+    }
+  }
+  return false;
+}
