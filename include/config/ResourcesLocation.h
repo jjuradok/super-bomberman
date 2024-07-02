edfdd7d06fa460252c00933ff33ed7c90886c6b4
Juan Manuel Jurado <102827797+jjuradok@users.noreply.github.com>
2024-07-02 09:43:45 -0300

    Add player 2 assets

--- include/config/ResourcesLocation.h
+++ include/config/ResourcesLocation.h
@@ -4,6 +4,7 @@
 using namespace std;
 
 const string PLAYER_TEXTURE = "assets/textures/player.png";
+const string SECOND_PLAYER_TEXTURE = "assets/textures/player2.png";
 const string DESTRUCTIBLE_BOX_TEXTURE="assets/textures/destructible_box.png";
 const string INDESTRUCTIBLE_BOX_TEXTURE = "assets/textures/indestructible_box.png";
 const string TEST_LEVEL_LAYOUT = "resources/levels/test.txt"; 
