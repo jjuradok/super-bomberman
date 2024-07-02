8861107080643349aa302fffb836cc04bca175d5
Juan Manuel Jurado <102827797+jjuradok@users.noreply.github.com>
2024-07-02 09:42:32 -0300

    Add players instances and load matrix method into partida class

--- include/Partida.h
+++ include/Partida.h
@@ -1,20 +1,24 @@
 #ifndef PARTIDA_H
 #define PARTIDA_H
-#include "Tanque.h"
+
 #include "Disparo.h"
 #include <vector>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include "Scene.h"
 #include "Level.h"
+#include "Tanque.h"
+
 using namespace sf;
 using namespace std;
 
 class Partida : public Scene
 {
-	Level level;
+	vector<vector<char>> matrix;
 	Tanque m_tanque_p1;
 	Tanque m_tanque_p2;
+	Level level;
 	vector<Disparo> m_disparos;
+	void loadMatrix(string fileName);
 public:
 	Partida();
 	void update(Game &j) override;
