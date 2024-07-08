b59782f5a803cd8d5fd8e88d7eed3a3a1d2dbeb3
Juan Manuel Jurado <102827797+jjuradok@users.noreply.github.com>
2024-07-08 10:07:06 -0300

    delete settings src from constants file

--- include/config/Config.h
+++ include/config/Config.h
@@ -4,12 +4,12 @@
 #include <string>
 using namespace std;
 
-const string SETTINGS_FILE = "resources/settings.txt";
 const int PLAYER_SPEED = 3;
 const float SCALE_FACTOR = 3.0f;
 const float PLAYER_SCALE_FACTOR = 2.65f;
 const float TILE_SIZE = 16.0f;
 const float PLAYER_TEXTURE_WIDTH = 15.0f;
 const float PLAYER_TEXTURE_HEIGHT = 24.0f;
+const float BOMB_LIFE_TIME = 3000.0f;
 
 #endif 
